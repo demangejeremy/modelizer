9695d1f24a15ff239e546f08db7ba289eb98c75f
Jérémy DEMANGE <j@lachainemedias.com>
2020-11-29 15:22:57 +0100

    Modifs logos

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,49 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+	<v-app>
+		<v-app-bar app color="red" dark>
+			<div class="d-flex align-center">
+				<a href="/">
+					<v-img
+						alt="Modelizer Logo"
+						class="shrink mr-2"
+						contain
+						:src="require('./assets/Modelizer.png')"
+						transition="scale-transition"
+						width="150"
+					/>
+				</a>
+			</div>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+			<v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+			<v-btn
+				href="https://github.com/demangejeremy/modelizer"
+				target="_blank"
+				text
+			>
+				<span class="mr-2">See on GitHub</span>
+				<v-icon>mdi-open-in-new</v-icon>
+			</v-btn>
+		</v-app-bar>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+		<v-main>
+			<HelloWorld />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+	import HelloWorld from "./components/HelloWorld";
 
-export default {
-  name: 'App',
+	export default {
+		name: "App",
 
-  components: {
-    HelloWorld,
-  },
+		components: {
+			HelloWorld,
+		},
 
-  data: () => ({
-    //
-  }),
-};
+		data: () => ({
+			//
+		}),
+	};
 </script>
